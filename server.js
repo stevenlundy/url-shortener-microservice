9f2323a47fba2a1eabc728c0b00c8f35cd0d1988
Steven Lundy <steven.lundy@gmail.com>
2016-01-19 20:21:11 -0500

    Add basic functionality to add urls

--- server.js
+++ server.js
@@ -2,6 +2,18 @@ var express = require('express');
 var port = process.env.PORT || 3000;
 
 var app = express();
+var sites = {};
+var counter = 1;
+
+app.get('/new/:url', function(req, res) {
+  if(!sites[req.params.url]) {
+    sites[req.params.url] = counter++;
+  }
+  res.send({
+    original_url: 'req.params.url',
+    short_url: sites[req.params.url]
+  })
+});
 
 app.get('/', function(req, res) {
   res.send('Hello World');
