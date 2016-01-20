60307294d4b250b7445ebdb9d4de722f6f294ce5
Steven Lundy <steven.lundy@gmail.com>
2016-01-19 20:26:11 -0500

    Add basic retrieving of urls

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ var port = process.env.PORT || 3000;
 
 var app = express();
 var sites = {};
+var short_ids = [];
 var counter = 1;
 
 app.get('/new/:url', function(req, res) {
   if(!sites[req.params.url]) {
-    sites[req.params.url] = counter++;
+    sites[req.params.url] = counter;
+    short_ids[counter++] = req.params.url;
   }
   res.send({
     original_url: 'req.params.url',
@@ -15,8 +17,14 @@ app.get('/new/:url', function(req, res) {
   })
 });
 
-app.get('/', function(req, res) {
-  res.send('Hello World');
+app.get('/:short_id', function(req, res) {
+  if(short_ids[req.params.short_id]) {
+    res.send('redirecting to ' + short_ids[req.params.short_id]);
+  } else {
+    res.send({
+      error: 'No short url found for given input'
+    });
+  }
 });
 
 app.listen(port, function() {
